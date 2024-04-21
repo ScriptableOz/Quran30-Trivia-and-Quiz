0df3f793a05ae302be52cf130710210fbc3524a0
ScriptableOz <165089349+ScriptableOz@users.noreply.github.com>
2024-04-21 17:32:02 +0800

    Theme Update Progress 1

---
+++ Assets/themeManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class themeManager : MonoBehaviour
+{
+    public Color primaryColor;
+    public Color secondaryColor;
+    public void changeTheme(string primaryPanel, string secondaryPanel)
+    {
+        print(primaryPanel);
+        print(secondaryPanel);
+    }
+}
